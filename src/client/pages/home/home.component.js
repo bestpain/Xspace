4eff33e1402919d17aa13b636be292914c3260cb
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-08-14 19:05:23 +0530

    Update home.component.js

--- src/client/pages/home/home.component.js
+++ src/client/pages/home/home.component.js
@@ -1,50 +1,58 @@
-import serialize from "serialize-javascript";
+import React from "react";
+import Filter from "../../components/filter/filter.component";
+import styled from "styled-components";
+import Results from "../../components/results/results.component";
+import {
+  fetchLaunchesRequest,
+  clearFilters,
+  clearLaunchesResults,
+} from "../../redux/actions";
+import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
 
-const helmet = Helmet.renderStatic();
+const ContentContainer = styled.div`
+display: grid;
+overflow: hidden;
+grid-template-columns: auto 1fr;
 
-const Html = ({ content, styles, store }) => `
-
-<style>
-*{
-  scrollbar-width: thin;
-  scrollbar-color: var(--bg-secondary) var(--bg-primary);
+@media screen and (max-width: 400px) {
+  grid-template-rows: auto;
+  grid-template-columns: auto;
 }
-  body{
-    background-color: rgb(241, 241, 241);
-    margin:0;
-    box-sizing:border-box;
-    font-size: 16px;
-    font-family: system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Ubuntu,Helvetica Neue,Oxygen,Cantarell,sans-serif;
-    font-weight: 400;
-    --transition-speed: 300ms;
-    --border: 0px;
-   
-  }
-  
-  #root{
-    height:100vh;
-    display: grid;
-    grid-template-rows: 40px 1fr 60px;
+`
+
+//Homepage
+class Home extends React.Component {
+  componentDidMount() {
+    // call the API and clear any redux state if present
+    this.props.clearLaunchesResults();
+    this.props.clearFilters();
+    this.props.fetchLaunchesRequest();
   }
 
-</style>
-  <html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-      ${helmet.title.toString()}
-      ${helmet.meta.toString()}
-      ${styles} 
-    </head>
-      <body>
-        <div id="root">${content}</div>
-        <script>
-          window.INITIAL_STATE=${serialize(store.getState())}
-        </script>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-`;
+  render() {
+    return (
+      <>
+        <Helmet>
+          <title>SpaceX REST API</title>
+          <meta
+            property="og:title"
+            content=" Open Source REST API for rocket, core, capsule, pad, and launch data "
+          />
+        </Helmet>
+        <ContentContainer>
+          <Filter />
+          <Results />
+        </ContentContainer>
+      </>
+    );
+  }
+}
 
-export default Html;
-
+export default {
+  component: connect(null, {
+    fetchLaunchesRequest,
+    clearFilters,
+    clearLaunchesResults,
+  })(Home),
+};
