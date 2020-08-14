7e0e6d21e923ffdaf8574bbfe4999ac0facc31b1
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-08-14 19:14:35 +0530

    Add files via upload

---
+++ src/client/components/footer/footer.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  text-align: center;
+  line-height: 60px;
+`;
+const Footer = () => {
+  return <FooterContainer>Developed By : Nitish Ranjan</FooterContainer>;
+};
+
+export default Footer;
